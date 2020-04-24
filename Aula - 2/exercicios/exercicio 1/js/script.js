48be52bdc54e6dc1b2a5df4f100e9f4186033ce7
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-04-23 22:02:56 -0300

    Resolução exercício 01

---
+++ Aula - 2/exercicios/exercicio 1/js/script.js
@@ -0,0 +1,19 @@
+/*
+  Exercício 1
+  -----------
+
+  A função insertion cria elementos no DOM e para isso recebe três argumentos, o elemento, o ID de um elemento
+  já existente no seu HTML e o texto que será inserido no elemento criado.   
+*/
+
+function insereElementoDeTexto(elemento,id,texto){
+  
+  const elementoNovo = document.createElement(elemento);
+  elementoNovo.innerText = texto;
+  const elementoExistente = document.querySelector(id);
+  elementoExistente.appendChild(elementoNovo);
+} 
+
+
+
+insereElementoDeTexto('p','#container',"Fui criado com Javascript"); 
